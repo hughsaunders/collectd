80ce5975aafb50dc1ddc1d0ecd955c3a6460fd84
Hugh Saunders <hugh@wherenow.org>
2013-05-15 16:50:06 +0100

    ensure do_package_upgrades flag is used

--- recipes/default.rb
+++ recipes/default.rb
@@ -25,7 +25,7 @@ platform_options = node["collectd"]["platform"]
 
 platform_options["collectd_packages"].each do |pkg|
   package pkg do
-    action :install
+    action node["osops"]["do_package_upgrades"] == true ? :upgrade : :install
     options platform_options["package_overrides"]
   end
 end
